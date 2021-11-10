40fa81c5b072130babfe01a42d78298acf875b9c
Lê Trung Kiên <91647989+kienltgcc19138@users.noreply.github.com>
2021-11-10 20:58:21 +0700

    Add new

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 
 
 <?php
-    //$conn = pg_connect("postgres://enesxkbnipglfn:ab5ea2e4b66767be64939974975f8a8e408c547b2d474887e57875666e04a13f@ec2-3-220-90-40.compute-1.amazonaws.com:5432/d5kcq00ts60umq");
-    $conn = pg_connect("postgresql://postgres:322001@localhost:5432/postgres");
+    $conn = pg_connect("postgres://shmtnrxqfepiky:dc2e01ef15220f14cbc81b522d51fceb224507f338f39cec83a996485e216a25@ec2-3-229-166-245.compute-1.amazonaws.com:5432/daqnh7pf14invs");
+    //$conn = pg_connect("postgresql://postgres:322001@localhost:5432/postgres");
     if(!$conn){
         die("Can not connect database");
     } 
